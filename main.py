c1081d736dc311fa23cd64f05f10bf1412a8e60b
Mark Mathews <mm2121@cam.ac.uk>
2018-09-11 17:30:10 +0100

    Check python interpreter version before running

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
+import sys
+
 from tictactoe import TicTacToe
 
 if __name__ == "__main__":
-    game = TicTacToe()
-    while True:
-        invalid_input = True
-        while invalid_input:
-            try:
-                print('Enter move position as m<space>n: ')
-                m, n = map(int, input().split())
-                invalid_input = False
-            except ValueError:
-                print('Invalid input - need two space separated integers\n')
-            except Exception as e:
-                print('Invalid input: {}\n'.format(e))
-        game.playMove(m, n)
-        if game.game_over:
-            print('Start new game? [Y/n]')
-            start_new_game = input()
-            if start_new_game.lower() == 'y':
-                print('\nStarting new game...\n')
-                game.newGame()
-            else:
-                print('\nThank you for playing!')
-                break
+    try:
+        assert sys.version_info >= (3, 0)
+    except AssertionError:
+        print('Please run this script using Python 3.0 or later')
+    else:
+        game = TicTacToe()
+        while True:
+            invalid_input = True
+            while invalid_input:
+                try:
+                    print('Enter move position as m<space>n: ')
+                    m, n = map(int, input().split())
+                    invalid_input = False
+                except ValueError:
+                    print('Invalid input - need two '
+                          'space separated integers\n')
+                except Exception as e:
+                    print('Invalid input: {}\n'.format(e))
+            game.playMove(m, n)
+            if game.game_over:
+                print('Start new game? [Y/n]')
+                start_new_game = input()
+                if start_new_game.lower() == 'y':
+                    print('\nStarting new game...\n')
+                    game.newGame()
+                else:
+                    print('\nThank you for playing!')
+                    break
